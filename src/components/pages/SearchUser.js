2c887cede8213316a236debf3637f1834736c2dc
vipul <vipul.infynno@gmail.com>
2022-12-21 11:35:09 +0530

    feat:design customer page

---
+++ src/components/pages/SearchUser.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AdminLayout } from '../layout/AdminLayout'
+
+const SearchUser = () => {
+    return (
+        <AdminLayout>
+            <div className='text-white'>SearchUser</div>
+        </AdminLayout>
+    )
+}
+export default SearchUser
